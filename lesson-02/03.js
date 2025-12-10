06a0ace19f8411b098a0daaf236e9306d6914101
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-11 01:21:49 +0300

    Complete Lesson-2 Task-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,28 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 100 // тестовое значение, можно изменять
 let grade
 // your code
+
+if (score >= 0 && score <= 49) {
+  grade = 'F'
+}
+
+if (score >= 50 && score <= 69) {
+  grade = 'D'
+}
+
+if (score >= 70 && score <= 79) {
+  grade = 'C'
+}
+
+if (score >= 80 && score <= 89) {
+  grade = 'B'
+}
+
+if (score >= 90 && score <= 100) {
+  grade = 'A'
+}
+
+console.log(grade)
