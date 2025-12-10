c6778c26cbc49b9793859ca7a7885f5869891658
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-11 01:11:28 +0300

    Fix Lesson-2 Task-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,7 +9,7 @@
 
 let dice1 = Math.floor(Math.random() * 6) + 1// your code
 let dice2 = Math.floor(Math.random() * 6) + 1// your code
-let isWinningDouble = dice1 === dice2// your code
+let isWinningDouble = (dice1 && dice2) > 3 && dice1 === dice2 // your code
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
