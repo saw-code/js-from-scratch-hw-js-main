87433804d67ad3ea9b31c9c8e0e5464e1952cd23
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-11 15:05:03 +0300

    fix Lesson-3 Task-3. Add 2 conditions

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,10 +2,11 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(num) {
-  if (num % 2 === 1 || num === 1) {
+  if (num % 2 === 1 || num === 1 || num % 2 === -1 || num === -1) {
     return false
   } else {
     return true
   }
 }
 
+console.log(isEven(-101))
