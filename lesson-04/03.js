b5ddc4213b735b8d4f0c80e4c9a0ae35585cd8cc
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-15 19:41:49 +0300

    Fix Lesson-4 Task-3

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -18,13 +18,6 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее.
 Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
-function includesElement(array, element) {
-  let includeElement
-  for (let i = 0; i < array.length; i++) {
-    array[i] === element ? includeElement = element : false
-  }
-  return includeElement === element
-}
 
 function getUnionArray (array1, array2) {
   let unionArray = []
