03a4904a59b5898d935759971cba0248ba258533
Dmitry Pilavskiy <saw.coding@gmail.com>
2025-12-15 19:39:27 +0300

    Fix Lesson-4 Task-3

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -18,21 +18,59 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее.
 Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
+function includesElement(array, element) {
+  let includeElement
+  for (let i = 0; i < array.length; i++) {
+    array[i] === element ? includeElement = element : false
+  }
+  return includeElement === element
+}
+
+function getUnionArray (array1, array2) {
+  let unionArray = []
+  for (let i = 0; i < array1.length; i++) {
+    unionArray.push(array1[i])
+  }
+
+  for (let i = 0; i < array2.length; i++) {
+    unionArray.push(array2[i])
+  }
+
+  return unionArray
+}
 
 function findCommonElements(array1, array2) {
+  let unionArray = getUnionArray(array1, array2)
+
   let result = [];
 
-  for (let i = 0; i < array1.length; i++) {
-    let element = array1[i];
+  for (let i = 0; i < unionArray.length; i++) {
+    let element = unionArray[i];
+    let count = 0;
 
-    if (includesElement(array2, element)) {
-      if (!includesElement(result, element)) {
-        result.push(element);
+    // Считаем, сколько раз элемент встречается в массиве
+    for (let j = 0; j < unionArray.length; j++) {
+      if (unionArray[j] === element) {
+        count++;
       }
+    }
+
+    // Если элемент встречается больше одного раза
+    // и ещё не добавлен в результат
+    let alreadyAdded = false;
+    for (let k = 0; k < result.length; k++) {
+      if (result[k] === element) {
+        alreadyAdded = true;
+        break;
+      }
+    }
+
+    if (count > 1 && !alreadyAdded) {
+      result.push(element);
     }
   }
 
   return result;
 }
 
-console.log(findCommonElements([1, 2, 3, 4, 5, 5], [ 4, 5, 6, 4, 8, 4, 1]))
+console.log(findCommonElements([1, 2, 3], [1, 4, 5, 6, 3]))
